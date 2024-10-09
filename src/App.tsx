4fa9a20e7f08e29a43223077cb41ab51e2cb7f6b
Sreekar33 <cs14btech11033@iith.ac.in>
2024-10-09 11:16:30 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import Headers from "./Header";
+
+let renderCount = 0;
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormValues>({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      age: 0
+    }
+  });
+  renderCount++;
+
+  console.log("errors", errors);
+
+  return (
+    <div>
+      <Headers
+        renderCount={renderCount}
+        description="Performant, flexible and extensible forms with easy-to-use validation."
+      />
+
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <input
+          {...register("firstName", {
+            required: {
+              value: true,
+              message: "this is required"
+            }
+          })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastName", {
+            maxLength: {
+              value: 5,
+              message: "Max length is 5"
+            }
+          })}
+          name="lastName"
+          placeholder="Last Name"
+        />
+
+        <input type="number" {...register("age", { valueAsNumber: true })} />
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
